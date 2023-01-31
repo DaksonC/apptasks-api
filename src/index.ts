77bd440124a49a09f529ede10f1b9b3ab5892734
Dakson Cruz <daksonc@gmail.com>
2023-01-31 09:39:05 -0300

    chore(instala o cors no projeto)

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,13 @@
 import express from "express";
-import { AppDataSource } from "./data-source";
 import routes from "./routes";
+import cors from "cors";
+import { AppDataSource } from "./data-source";
 
 AppDataSource.initialize().then(() => {
   const app = express();
 
+  app.use(cors());
   app.use(express.json());
-
   app.use(routes)
 
   return app.listen(process.env.PORT, () => {
